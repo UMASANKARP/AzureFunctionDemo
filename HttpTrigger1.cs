3fd31078e2590a5c15e7351f337d2880db405f28
Umasankar <Umasankarpuligundla@gmail.com>
2024-07-01 12:08:05 -0400

    opentelemetry

--- HttpTrigger1.cs
+++ HttpTrigger1.cs
@@ -1,29 +1,29 @@
-using Microsoft.Azure.Functions.Worker;
-using Microsoft.Azure.Functions.Worker.Http;
-using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 
-namespace Company.Function
+namespace MyFunctionApp
 {
-    public class HttpTrigger1
+    public static class HttpTriggerFunction
     {
-        private readonly ILogger<HttpTrigger1> _logger;
-
-        public HttpTrigger1(ILogger<HttpTrigger1> logger)
+        [FunctionName("HttpTriggerFunction")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
         {
-            _logger = logger;
-        }
+            log.LogInformation("C# HTTP trigger function processed a request.");
 
-        [Function("HttpTrigger1")]
-        public async Task<HttpResponseData> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
-        {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            string responseMessage = "This is the real world practice 1";
 
-            var response = req.CreateResponse(System.Net.HttpStatusCode.OK);
-            await response.WriteStringAsync("Hello World this is Uma Shankar Demo part1-12");
-
-            return response;
+            return new ContentResult
+            {
+                Content = responseMessage,
+                ContentType = "text/html",
+                StatusCode = 200
+            };
         }
     }
 }
